f45941a7c8204791e89b6b1066d3b130831f70e7
Al Wold <a@trusona.com>
2018-08-01 17:03:40 -0700

    Add user integration test that doesn't work, comment out for now

---
+++ TrusonaSDK.Test/Integration/UserServiceIntegrationTest.cs
@@ -0,0 +1,35 @@
+﻿//
+// UserServiceIntegrationTest.cs
+//
+// Author:
+//       alwold <>
+//
+// Copyright (c) 2018 
+//
+//
+using System;
+using Xunit;
+using TrusonaSDK.API;
+using FluentAssertions;
+
+namespace TrusonaSDK.Test.Integration
+{
+  public class UserServiceIntegrationTest : IntegrationServiceTest
+  {
+    //[Fact]
+    [Trait("Category", "Integration")]
+    public void DeleteUser_should_return_a_valid_response()
+    {
+      //given
+      var userIdentifier = "abc123";
+      var deviceIdentifier = "kC_9iF_CNcJqdU4PvJspx6okdQnxJsYNteL0EJG_O-c";
+      sut.CreateUserDevice(userIdentifier, deviceIdentifier).Wait();
+
+      //when
+      Action action = () => { sut.DeleteUser(userIdentifier).Wait(); };
+
+      //then
+      action.Should().NotThrow();
+    }
+  }
+}
