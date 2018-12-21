11a65ddb4e6ed530e5403989aa89a5b55106fb1a
Ryan Wilson <r@trusona.com>
2018-12-21 10:41:44 -0700

    Moved GetConfiguration method out of the Trusona class to get it under test and added tests

---
+++ TrusonaSDK.API/Configuration/ConfigurationFactory.cs
@@ -0,0 +1,30 @@
+﻿//
+// ConfigurationFactory.cs
+//
+// Author:
+//       delduggel <>
+//
+// Copyright (c) 2018 
+//
+//
+using TrusonaSDK.HTTP;
+using TrusonaSDK.HTTP.Environment;
+
+namespace TrusonaSDK.API.Configuration
+{
+  public class ConfigurationFactory
+  {
+    public IConfiguration GetConfiguration(TrusonaEnvironment environment, string token, string secret)
+    {
+      switch (environment)
+      {
+        case TrusonaEnvironment.UAT:
+          return new UATEnvironment(token, secret);
+
+        case TrusonaEnvironment.PRODUCTION:
+        default:
+          return new ProductionEnvironment(token, secret);
+      }
+    }
+  }
+}
