c07c7c6ef65aa8ae423a38593c30e12100f13326
Ryan Wilson <r@trusona.com>
2018-12-21 11:14:06 -0700

    Refactored switch to use a dictionary of endpoints

--- TrusonaSDK.API/Configuration/ConfigurationFactory.cs
+++ TrusonaSDK.API/Configuration/ConfigurationFactory.cs
@@ -7,30 +7,25 @@
 // Copyright (c) 2018 
 //
 //
+using System.Collections.Generic;
 using TrusonaSDK.HTTP;
 
 namespace TrusonaSDK.API.Configuration
 {
   public class ConfigurationFactory
   {
-    private static readonly string PRODUCTION_ENDPOINT = "https://api.trusona.net";
-    private static readonly string UAT_ENDPOINT = "https://api.staging.trusona.net";
-    private static readonly string AP_PRODUCTION_ENDPOINT = "https://api.ap.trusona.net/";
+    private static readonly Dictionary<TrusonaEnvironment, string> endpoints = new Dictionary<TrusonaEnvironment, string>()
+    {
+      { TrusonaEnvironment.PRODUCTION, "https://api.trusona.net" },
+      { TrusonaEnvironment.UAT, "https://api.staging.trusona.net"},
+      { TrusonaEnvironment.AP_PRODUCTION, "https://api.ap.trusona.net/"}
+    };
 
     public IConfiguration GetConfiguration(TrusonaEnvironment environment, string token, string secret)
     {
-      switch (environment)
-      {
-        case TrusonaEnvironment.UAT:
-          return new Configuration(UAT_ENDPOINT, token, secret);
+      var endpoint = endpoints[environment];
 
-        case TrusonaEnvironment.AP_PRODUCTION:
-          return new Configuration(AP_PRODUCTION_ENDPOINT, token, secret);
-
-        case TrusonaEnvironment.PRODUCTION:
-        default:
-          return new Configuration(PRODUCTION_ENDPOINT, token, secret);
-      }
+      return new Configuration(endpoint, token, secret);
     }
   }
 }
