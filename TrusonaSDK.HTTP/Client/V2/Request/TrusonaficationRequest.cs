92affef32daa4a6f87391e2d075bea9d55a89541
Nikolas Mangu-Thitu <n@trusona.com>
2020-12-12 09:56:53 -0800

    add fields on the http side too

--- TrusonaSDK.HTTP/Client/V2/Request/TrusonaficationRequest.cs
+++ TrusonaSDK.HTTP/Client/V2/Request/TrusonaficationRequest.cs
@@ -9,25 +9,39 @@
 using System;
 using System.Collections.Generic;
 using Newtonsoft.Json;
+using Newtonsoft.Json.Serialization;
 
 namespace TrusonaSDK.HTTP.Client.V2.Request
 {
+  [JsonObject(NamingStrategyType = typeof(SnakeCaseNamingStrategy), ItemNullValueHandling = NullValueHandling.Ignore)]
   public class TrusonaficationRequest : BaseRequestResponse
   {
     public string DeviceIdentifier { get; set; }
+    
     public string UserIdentifier { get; set; }
+    
     [JsonProperty("trucode_id")]
     public string TruCodeId { get; set; }
+    
     [JsonProperty("email")]
     public string EmailAddress { get; set; }
+    
     public int DesiredLevel { get; set; }
+    
     public string Action { get; set; }
+    
     public string Resource { get; set; }
+    
     public DateTime? ExpiresAt { get; set; }
+    
     public bool UserPresence { get; set; }
+    
     public bool Prompt { get; set; }
+    
     public bool ShowIdentityDocument { get; set; }
+    
     public Dictionary<string, object> CustomFields { get; set; }
+    
     public string CallbackUrl { get; set; }
 
     public TrusonaficationRequest() { }
--- TrusonaSDK.HTTP/Client/V2/Response/TrusonaficationResponse.cs
+++ TrusonaSDK.HTTP/Client/V2/Response/TrusonaficationResponse.cs
@@ -5,17 +5,28 @@
 //       David Kopack <d@trusona.com>
 //
 // Copyright (c) 2018 Trusona, Inc.
+using Newtonsoft.Json;
+using Newtonsoft.Json.Serialization;
+
 using System;
+
 using TrusonaSDK.HTTP.Client.V2.Request;
 
 namespace TrusonaSDK.HTTP.Client.V2.Response
 {
+  [JsonObject(NamingStrategyType = typeof(SnakeCaseNamingStrategy), ItemNullValueHandling = NullValueHandling.Ignore)]
   public class TrusonaficationResponse : TrusonaficationRequest
   {
     public Guid Id { get; set; }
+
     public string Status { get; set; }
+
     public DateTime CreatedAt { get; set; }
+
     public DateTime UpdatedAt { get; set; }
+
+    public string TrusonaId { get; set; }
+
     public TrusonaficationResultResponse Result { get; set; }
   }
 }
